ceaf258291a8a35b9d48debbd3ecc4fa59a26632
purush2300 <reddypurushothm@gmail.com>
2026-05-08 20:48:21 +0530

    Initial commit

--- New2.java
+++ New2.java
@@ -1,16 +1,13 @@
 import java.util.*;
 public class New2 {
     public static void main(String[] args) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int[]nums={1,2,1,2,1,2,3,1,3,2};
-        int k=2;
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-           if( map.size()==k){
-            System.out.println(map.keySet());
-            return;
-            }
-        }
+        HashMap<Character,Integer> map=new HashMap<>();
+       String a="aabbaaccc";
+       for (int i = 0; i < a.length(); i++) {
+        char c=a.charAt(i);
+        map.put(c, map.getOrDefault(c, 0)+1);
+       }
+       System.out.println(map);
        
     }
 }
