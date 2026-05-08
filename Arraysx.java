43ed3d27c56ba1a02c4fa656ec2aef1d8055e661
purush2300 <reddypurushothm@gmail.com>
2026-05-08 22:20:01 +0530

    Initial commit

---
+++ Arraysx.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Arraysx {
+    public static void main(String[] args) {
+        int[]a={1,2};
+        int[]b={2,1};
+Arrays.toString(a);
+System.out.println(a);
+
+
+    }
+}
--- New.java
+++ New.java
@@ -1,16 +1,11 @@
 import java.util.*;
-
+import java.util.Arrays;
 public class New {
     public static void main(String[] args) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int[]nums={3,2,3};
-        int k=nums.length;
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i],i);
-            if(map.get(nums[i])>k/2){
-                System.out.println(nums[i]);
-            }
-    }
+          int[]a={1,2};
+        int[]b={2,1};
+
+System.out.println(Arrays.toString(a));
     
     }
 }
