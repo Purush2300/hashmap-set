efbe7e06fc3f2fc4cdecd3f27da6bd50d925bb5b
purush2300 <reddypurushothm@gmail.com>
2026-05-09 07:02:22 +0530

    Initial commit

--- New.java
+++ New.java
@@ -2,18 +2,31 @@ import java.util.*;
 import java.util.Arrays;
 public class New {
     public static void main(String[] args) {
-    int []a={0,0,3,3,5,6};
-    int j=0;
-    for (int i = 0; i < a.length; i++) {
-        if(a[i]!=a[j]){
-            j++;
-            int temp=a[i];
-            a[i]=a[j];
-            a[j]=temp;
+    int []a={1,2,3,4,5};
+  int k=2;
+int n=a.length;
+k=k%n;
+int[]temp=new int[k];
+
+for (int i = 0; i < k; i++) {
+    temp[i]=a[i];
+}
+for (int i = k; i < n; i++) {
+    a[i-k]=a[i];
+}
+
+
+    for (int i = 0; i < k; i++) {
+       
+           a[n-k+1] =temp[i];
         }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+        
 
     }
-    System.out.println(j+1);
+   
    
     }
-}
+
