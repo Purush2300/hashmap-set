2b4dbabc22cceb1b995ad00cd87a6180cd0c25df
purush2300 <reddypurushothm@gmail.com>
2026-05-10 21:45:36 +0530

    Initial commit

--- New2.java
+++ New2.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class New2 {
     public static void main(String[] args) {
         HashMap<Character,Integer> map=new HashMap<>();
-       String a="aabbaaccc";
+       String a="aabbaac";
        for (int i = 0; i < a.length(); i++) {
         char c=a.charAt(i);
         map.put(c, map.getOrDefault(c, 0)+1);
