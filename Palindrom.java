e6ae8988e30d15ce0681331623f69a96132793e9
purush2300 <reddypurushothm@gmail.com>
2026-05-11 22:16:16 +0530

    Initial commit

---
+++ Palindrom.java
@@ -0,0 +1,15 @@
+public class Palindrom {
+    public static void main(String[] args) {
+        String s="  hello world  ";
+       s= s.trim();
+        String[]arr=s.split("\\s+");
+        String ans="";
+       for(int i=arr.length-1;i>=0;i--){
+            ans+=arr[i];
+            if(i!=0){
+                ans+=" ";
+            }
+       }
+       System.err.println(ans);
+    }
+}
