e4e20f70a164611a137709e901d9156eb5855ba8
purush2300 <reddypurushothm@gmail.com>
2026-05-14 07:21:42 +0530

    Initial commit

--- Subarray.java
+++ Subarray.java
@@ -2,44 +2,35 @@ import java.util.HashMap;
 
 public class Subarray {
  public static void main(String[] args) {
-    HashMap<Character, Character> map1=new HashMap<>();
-    HashMap<Character, Character> map2=new HashMap<>();
-    String s="foo";
-    String t="bag";
-
+    HashMap<Character, Integer> map=new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+       
+String s= "MCDX";
+    int total=0;
     for(int i=0;i<s.length();i++){
-        char c1=s.charAt(i);
-        char c2=t.charAt(i);
-        if(map1.containsKey(c1)){
-            if(map1.get(c1)!=c2){
-               System.err.println("false");
-               return;
+        int cur=map.get(s.charAt(i));
+        if(i<s.length()-1){
+            int next=map.get(s.charAt(i+1));
+            if(cur<next){
+                total-=cur;
+            }
+            else{
+                total+=cur;
             }
         }
         else{
-            map1.put(c1, c2);
+            total+=cur;
         }
-
-        if(map2.containsKey(c2)){
-            if(map2.get(c2)!=c1){
-               System.err.println("false");
-               return;
-            }
-        }
-        else{
-            map2.put(c2, c1);
-        }
-
-
-
-
     }
 
-System.err.println("true");
-return;
 
-
-
+System.err.println(total);
 
  }   
 }
