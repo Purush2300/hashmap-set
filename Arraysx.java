ed4589f2c85ce24b5a8d5f73df95fef2ec1c28a8
purush2300 <reddypurushothm@gmail.com>
2026-05-14 09:12:02 +0530

    Initial comit

--- Arraysx.java
+++ Arraysx.java
@@ -1,12 +1,29 @@
-import java.util.Arrays;
 
 public class Arraysx {
     public static void main(String[] args) {
-        int[]a={1,2};
-        int[]b={2,1};
-Arrays.toString(a);
-System.out.println(a);
+        int []arr={2,2,2,3,1,1,2,4,4,4,3,3};
 
-
+        int maxfreq=Integer.MIN_VALUE;
+        int minfreq=Integer.MAX_VALUE;
+        int maxele=0;
+        int minele=0;
+        for(int i=0;i<arr.length;i++){
+            int cnt=0;
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    cnt++;
+                }
+            }
+            if(cnt>maxfreq){
+                maxele=arr[i];
+                maxfreq=cnt;
+                }
+                if(cnt<minfreq){  
+                    minele=arr[i];
+                    minfreq=cnt;
+                }
+        }
+        System.err.println(maxele+" ->"+maxfreq);
+        System.err.println(minele+"-> "+minfreq);
     }
 }
