9ad0fd4de6c37bee84533b9cab6f1a3e6760618f
purush2300 <reddypurushothm@gmail.com>
2026-05-17 21:20:33 +0530

    Initial commit

--- Revision.java
+++ Revision.java
@@ -1,35 +1,16 @@
 
-import java.util.*;
 public class Revision {
     public static void main(String[] args) {
-        String s="aa";
-        String t="ba";
-       HashMap<Character,Character> map1=new HashMap<>();
-        HashMap<Character,Character> map2=new HashMap<>();
-        for(int i=0;i<s.length();i++){
-            char c1=s.charAt(i);
-            char c2=t.charAt(i);
-        if(map1.containsKey(c1)){
-            if(map1.get(c1)!=c2){
-                System.out.println("False");
-                return ;
+        String []s={"flower","flow","flight"};
+        String p=s[0];
+        for(int i=0;i<p.length();i++){
+            char ch=p.charAt(i);
+            for(int j=1;j<s.length;j++){
+                if(i>s[j].length()||s[j].charAt(i)!=ch){
+                    System.err.println(p.substring(0,i));
+                }
             }
         }
-        else{
-                map1.put(c1, c2);
-            }
-
-        if(map2.containsKey(c2)){
-            if(map2.get(c2)!=c1){
-                System.out.println("false");
-                return;
-            }
-        }
-        else{
-                map2.put(c2, c1);
-            }
-
-        }
-        System.err.println("True");
+      
     }
 }
