ce09c3e0f904c95148b05a5a077f322eee895ea6
purush2300 <reddypurushothm@gmail.com>
2026-05-18 22:05:12 +0530

    initial commit

--- Sub.java
+++ Sub.java
@@ -1,15 +1,32 @@
 public class Sub {
     public static void main(String[] args) {
-        String max="";
-        String s="abcabcewd";
-        for(int i=0;i<s.length();i++){
-            for(int j=i+1;j<=s.length();j++){
-               String sub=s.substring(i,j);
-               if(sub.length()>max.length()){
-                max=sub;
-               }
+       String ma="";
+        String s="ababa";
+            for(int i=0;i<s.length();i++){
+                for(int j=i+1;j<=s.length();j++){
+                    if(palindrome(s,i,j)){
+                        String sub=s.substring(i,j);
+                        if(sub.length()>ma.length()){
+                            ma=sub;
+                        }
+                    }
+                }
             }
+System.err.println(ma);
+        
+        
+    }
+
+    private static boolean  palindrome(String s,int i,int j) {
+      
+       while(i<j){
+        if(s.charAt(i)!=s.charAt(j)){
+            
+            return false;
         }
-        System.err.println(max);
+        i++;
+        j--;
+       }
+     return true;
     }
 }
