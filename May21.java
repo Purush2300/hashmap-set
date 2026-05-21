ec8225261418d2ac334af87962ca72f78a15f671
purush2300 <reddypurushothm@gmail.com>
2026-05-21 22:17:16 +0530

    Initial commit

---
+++ May21.java
@@ -0,0 +1,23 @@
+public class May21 {
+    public static void main(String[] args) {
+        int n=6;
+        int i=1;
+        int j=n;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(mid*mid==n){
+                System.out.println(mid);
+                return;
+            }
+            else if(mid*mid<n){
+                i=mid+1;
+            }
+            else{
+                j=mid-1;
+            }
+        }
+        System.err.println(i);
+         System.err.println(j);
+         
+    }
+}
--- Peak.java
+++ Peak.java
@@ -6,14 +6,14 @@ public class Peak {
 
     private static boolean  find(int[] a) {
         int n=a.length;
-        if(n==1) return true;
-        if(a[0]>a[1]) return true;
-        if(a[n-1]>a[n-2]) return true;;
+        if(n==1) return 0;
+        if(a[0]>a[1]) return 1;
+        if(a[n-1]>a[n-2]) return n-1;;
         int i=1;int j=n-2;
         while(i<=j){
             int mid=(i+j)/2;
             if(a[mid]>a[mid+1] && a[mid]>a[mid-1]){
-                return true;
+                return 1;
             }
             if(a[mid]>a[mid-1]){
                 i=mid+1;
@@ -22,6 +22,6 @@ public class Peak {
                 j=mid-1;
             }
         }
-        return false;
+        return 0;
     }
 }
