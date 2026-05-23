84f987dde9439b9ffeb58035521571b374445ec1
purush2300 <reddypurushothm@gmail.com>
2026-05-23 21:12:30 +0530

    Initial commit

--- May23.java
+++ May23.java
@@ -1,27 +1,32 @@
+
+
 public class May23 {
     public static void main(String[] args) {
-        int []a={1,2,3,4,5};
-        int n=a.length;
-        int k=2;
-      
-        reverse(a,0,k-1);
-        reverse(a,k,n-1);
-        reverse(a,0,n-1);
-        for(int i=0;i<a.length;i++){
-            System.out.print(a[i]+" ");
-        }
-
-    }
-
-    private static void reverse(int[] a, int i, int j) {
-        
-       while(i<j){
-        int y=a[i];
-        a[i]=a[j];
-        a[j]=y;
-        i++;
-        j--;
-       }
        
+               int[]a={10,5,2,7,1,9};
+             
+               int k=15;
+               int maxlen=0;
+               
+               for(int i=0;i<a.length;i++){
+                        
+                int sum=0;
+                for(int j=i;j<a.length;j++){
+                    sum+=a[j];
+                    if(sum==k){
+                        maxlen=Math.max(maxlen, j-i+1);
+                    }
+                }
+
+
+                   
+               }
+        System.out.println(maxlen);
+        }
+        
+
     }
-}
+
+       
+    
+
