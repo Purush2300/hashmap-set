21a5bfdb2527864d9ccced23d3101ddb74a0f69b
purush2300 <reddypurushothm@gmail.com>
2026-06-07 10:56:16 +0530

    Initial commit

--- june7.java
+++ june7.java
@@ -9,14 +9,14 @@ public class june7 {
         int []temp=new int[k];
 
         for(int i=0;i<k;i++){
-               temp[i]=arr[n-k+i]; 
+              temp[i]=arr[i];
         }
    
-        for(int i=n-k-1;i>=0;i--){
-           arr[i+k] =arr[i];
+        for(int i=k;i<n;i++){
+        arr[i-k]=arr[i];
         }
         for(int i=0;i<k;i++){
-       arr[i]=temp[i];
+            arr[n-k+i]=temp[i];
         }
         for(int i=0;i<arr.length;i++){
        System.out.print(arr[i]+" ");
