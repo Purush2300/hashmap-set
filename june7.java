88863529c802c7cb1404fa75c2bd2778b256e116
purush2300 <reddypurushothm@gmail.com>
2026-06-07 12:23:44 +0530

    Initial commit

--- june7.java
+++ june7.java
@@ -1,35 +1,24 @@
 public class june7 {
     public static void main(String[] args) {
         
-        int[]arr1={1,2,3,4,5};
-        int[] arr2={1,2,3,6,7,8};
-        int a=arr1.length;
-        int b=arr2.length;
+        int[]arr={1,1,0,1,1,1,0,0,1,1,1,1,1};
+       
     
-        int []temp=new int[a+b];
-        int k=0;
-        int i=0;
-        int j=0;
-        while(i<a && j<b){
-            if(arr1[i]<arr2[j]){
-                temp[k++]=arr1[i];
-            }
-            else{
-                temp[k++]=arr2[j];
-            }
-            i++;
-            j++;
+        
+       
+        int max=0;
+        int curr=0;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]!=0){
+            curr++;
+            max=Math.max(curr, max);
 
         }
-        while(i<a){
-            temp[k++]=arr1[i++];
+        else{
+            curr=0;
         }
-        while(j<b){
-            temp[k++]=arr2[j++];
-        }
-        for(int p=0;p<temp.length;p++){
-            System.out.print(temp[p]+" ");
-        }
+    }
+       System.out.println(max);
     }
 
 }
