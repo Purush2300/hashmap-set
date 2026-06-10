6c246ead258aa760777fc6566ec80025f45d3d4b
purush2300 <reddypurushothm@gmail.com>
2026-06-10 20:01:51 +0530

    Initilal commit

---
+++ June10.java
@@ -0,0 +1,19 @@
+public class June10 {
+    public static void main(String[] args) {
+        int[]arr={8,7,6,4,1};
+        int i=0;
+        int j=arr.length-1;
+        while(i<=j){
+            if(arr[i]*arr[j]==36){
+                System.out.println(arr[i]+" "+arr[j]);
+                return;
+            }
+            else if(arr[i]*arr[j]>36){
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+    }
+}
--- june7.java
+++ june7.java
@@ -1,24 +1,24 @@
+
+import java.util.HashMap;
+
+
+
 public class june7 {
     public static void main(String[] args) {
         
-        int[]arr={1,1,0,1,1,1,0,0,1,1,1,1,1};
-       
-    
-        
-       
-        int max=0;
-        int curr=0;
-    for(int i=0;i<arr.length;i++){
-        if(arr[i]!=0){
-            curr++;
-            max=Math.max(curr, max);
-
+        int[]arr={3,2,3};
+        int k=arr.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+            if(map.get(arr[i])>k/2){
+                System.out.println(arr[i]);
+            }
         }
-        else{
-            curr=0;
-        }
-    }
-       System.out.println(max);
+       
+            
+   
+      
     }
 
 }
