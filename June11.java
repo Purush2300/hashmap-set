ca3028b5b5a58b8b0e19e1cb568c7bba7a86eaf3
purush2300 <reddypurushothm@gmail.com>
2026-06-11 22:52:59 +0530

    Initial commit

--- June11.java
+++ June11.java
@@ -1,23 +1,36 @@
 public class June11 {
     public static void main(String[]args){
-        int[]arr={1,2,3,4,5};
-        int n=arr.length-1;
-      int i=0;
-      int j=n;
+        int[]arr={2,3,1,6,5,4};
+        int i=arr.length-2;
         
-      while(i<=j){
-        int mid=(i+j)/2;
+        while(i>=0 && arr[i]>arr[i+1]){
+            i--;
+        }
+           if(i>=0){
+             int j=arr.length-1;
+            while(arr[j]<=arr[i]){
+                j--;
+            }   
+           
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            
+        }
+        reverse(arr,i+1,arr.length-1);
+        
+        for(int k=0;k<arr.length;k++){
+            System.out.print(arr[k]+" ");
+        }
+    }
 
-        if(i==0||arr[i-1]<arr[mid] && i==n||arr[mid]>arr[i+1]){
-            System.out.println(mid);
-            return;
+    private static void reverse(int[] arr, int i, int j) {
+        while(i<=j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
         }
-        else if(arr[i]>arr[i-1]){
-            i=mid+1;
-        }
-        else{
-            j=mid-1;
-        }
-      }
     }
 }
