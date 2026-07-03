6ded37656e9ab197b87d424cca03205caac34fcb
purush2300 <reddypurushothm@gmail.com>
2026-07-03 13:39:36 +0530

    Initial commit

--- July3.java
+++ July3.java
@@ -1,24 +1,25 @@
 
+import java.util.HashMap;
+
+
 
 public class July3{
 public static void main(String[] args) {
     int[]arr={1,2,4,3,2,1};
     int k=3;
-    int prefix=0;
     int cnt=0;
-    for(int i=0;i<arr.length;i++){
-        prefix+=arr[i];
-        for(int j=i+1;j<arr.length;j++){
-            if(prefix+arr[j]==k){
-                cnt++;
-            }
-        }
-        if(arr[i]==k){
-            cnt++;
-        }
+   HashMap <Integer,Integer>map=new HashMap<>();
+int prefix=0;
+map.put(0,1);
+for(int i=0;i<arr.length;i++){
+    prefix+=arr[i];
+    if(map.containsKey(prefix-k)){
+        cnt+=map.get(prefix-k);
     }
-   System.out.println(cnt);
-  
+    map.put(prefix, map.getOrDefault(prefix, 0) + 1);
+}
+     
+  System.out.println(cnt);
 
 }
 }
