a7b84e2b4d7e7d0ab27ec209fa6e9a5b77cc39d4
purush2300 <reddypurushothm@gmail.com>
2026-07-03 13:09:09 +0530

    Initial commit

--- July3.java
+++ July3.java
@@ -2,27 +2,23 @@
 
 public class July3{
 public static void main(String[] args) {
-    int[]arr={3, 2, 3, 2, 4, 3};
-    int l=0;
-    int r=4;
-    int[]prefix=new int[arr.length];
-    prefix[0]=arr[0];
-    for(int i=1;i<arr.length;i++){
-            prefix[i]=prefix[i-1]+arr[i];
+    int[]arr={1,2,4,3,2,1};
+    int k=3;
+    int prefix=0;
+    int cnt=0;
+    for(int i=0;i<arr.length;i++){
+        prefix+=arr[i];
+        for(int j=i+1;j<arr.length;j++){
+            if(prefix+arr[j]==k){
+                cnt++;
+            }
+        }
+        if(arr[i]==k){
+            cnt++;
+        }
     }
-    int sum=0;
-    if(l==0){
-         sum=prefix[r];
-         System.out.println(sum);
-    }
-    else{
-           sum=prefix[r]-prefix[l-1];
-           System.out.println(sum);
-    }
+   System.out.println(cnt);
   
-for(int i=0;i<prefix.length;i++){
-    System.out.println(prefix[i]);
-}
 
 }
 }
