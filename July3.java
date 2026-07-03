a0e42a0ac3f92a6e38b6c1be3c8e3299a163d542
purush2300 <reddypurushothm@gmail.com>
2026-07-03 09:17:43 +0530

    Initial commit

--- July3.java
+++ July3.java
@@ -1,25 +1,28 @@
 
+import static java.lang.Math.abs;
 import java.util.HashMap;
 
 public class July3{
 public static void main(String[] args) {
-    int[]arr={3,2,1,2,5};
-    int k=4;
-    countPair(arr,k);
+    int[]arr={1,2,3,1,2,3};
+    int k=2;
+    System.out.println(countPair(arr,k));
 }
 
-    private static void countPair(int[] arr, int k) {
-        int cnt=0;
+    private static boolean  countPair(int[] arr, int k) {
+       
      
        HashMap<Integer,Integer> map=new HashMap<>();
       
         for(int i=0;i<arr.length;i++){
-        
-            if(map.containsKey(k-arr[i])){
-                cnt++;
+            if(map.containsKey(arr[i])){
+                int prev=map.get(arr[i]);
+                if(abs(i-prev)<=k){
+                    return true;
+                }
             }
-            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        map.put(arr[i],i);
         }
-        System.out.println(cnt);
+        return false;
     }
 }
