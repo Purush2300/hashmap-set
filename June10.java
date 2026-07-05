3dc9f073f7bc2f2ef8897bf33222b98bbc4972b6
purush2300 <reddypurushothm@gmail.com>
2026-07-05 21:09:39 +0530

    Innitial commit

--- June10.java
+++ June10.java
@@ -1,6 +1,6 @@
 public class June10 {
     public static void main(String[] args) {
-        int[]arr={8,7,6,4,1};
+        int[]arr={8,7,6,4,11,11};
         int i=0;
         int j=arr.length-1;
         while(i<=j){
