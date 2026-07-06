7c60485a16da9c3d9df89cfff6634dbc21c2d838
purush2300 <reddypurushothm@gmail.com>
2026-07-06 22:27:11 +0530

    Initial commit

--- July6.java
+++ July6.java
@@ -1,20 +1,19 @@
 public class July6 {
     public static void main(String[] args) {
-        int[]arr={3,1,3,-2,2,2,1,1,2,1,1,1,1};
-        int k=4;
-        int len=0;
-        
-        for(int i=0;i<arr.length;i++){
-            int sum=arr[i];
-            for(int j=i;j<arr.length;j++){
-                sum+=arr[j];
-                if(sum==k && j-i+1>len){
-                   
-                        len=j-i+1;
-                    
-                }
-            }
+       String s="a";
+       String a="";
+       int i=0;
+       int j=i+1;
+       int len=0;
+       while(i<s.length() && j<s.length()){
+        char c1=s.charAt(i);
+        char c2=s.charAt(j);
+        if(c1==c2){
+            i++;
         }
-        System.out.println(len);
+        len=Math.max(len, j-i+1);
+        j++;
+       }
+       System.out.println(len);
     }
 }
