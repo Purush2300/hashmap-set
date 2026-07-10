889f46ccde4cca87a007882eac69a9c3c89096d5
purush2300 <reddypurushothm@gmail.com>
2026-07-10 21:35:18 +0530

    Initial commit

--- July9kodnest.java
+++ July9kodnest.java
@@ -1,15 +1,6 @@
 public class July9kodnest {
  public static void main(String[] args) {
-     String s="welcome to my payroll     ";
-     int i=s.length()-1;
-     int len=0;
-     while(i>=0 && s.charAt(i)==' '){
-        i--;
-     }
-     while(i>=0 && s.charAt(i)!=' '){
-        len++;
-        i--;
-     }
-     System.out.println(len);
+    int[]arr= {-1,0,1,2,-1,-4};
+    
  }   
 }
