0f492a12672563852a2762eb67b661c64f24e6d0
purush2300 <reddypurushothm@gmail.com>
2026-07-12 20:22:53 +0530

    INitial commit

--- Subarray.java
+++ Subarray.java
@@ -7,7 +7,7 @@ public class Subarray {
         map.put('V', 5);
         map.put('X', 10);
         map.put('L', 50);
-        map.put('C', 100);
+        map.put('C', 1);
         map.put('D', 500);
         map.put('M', 100);
        
