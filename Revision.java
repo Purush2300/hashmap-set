b02a59ac7704c7b53b8c60ed2760f2241f8366c4
purush2300 <reddypurushothm@gmail.com>
2026-07-28 20:46:28 +0530

    i

--- Revision.java
+++ Revision.java
@@ -1,7 +1,7 @@
 
 public class Revision {
     public static void main(String[] args) {
-        String []s={"flower","flight"};
+        String []s={"flower","floe"};
         String p=s[0];
         for(int i=0;i<p.length();i++){
             char ch=p.charAt(i);
