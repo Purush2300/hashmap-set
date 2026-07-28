3bc0e62c4793ee945ee85da7aab31e9d854ba650
purush2300 <reddypurushothm@gmail.com>
2026-07-28 20:44:32 +0530

    Initial commit

--- Minimum.java
+++ Minimum.java
@@ -1,6 +1,6 @@
 public class Minimum {
     public static void main(String[] args) {
-        int[]a={5,6,7,1,2,3};
+        int[]a={5,6,7,1,2,10};
         int dex=0;
         int min=Integer.MAX_VALUE;
         int i=0;int j=a.length-1;
